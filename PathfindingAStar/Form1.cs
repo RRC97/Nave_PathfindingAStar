bb0496ead2a557ec82e518e704e7245a5dbb7b11
unknown <seeduc@NAVE036-9J7KITP.nave.local>
2015-05-13 14:21:01 -0300

    first

---
+++ PathfindingAStar/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace PathfindingAStar
+{
+    public partial class Form1 : Form
+    {
+        Thread thread;
+        Space[] spaces;
+        Space a, b;
+        int rows, columns;
+        public Form1()
+        {
+            InitializeComponent();
+
+            Paint += new PaintEventHandler(onDraw);
+
+            rows = 10;
+            columns = 15;
+
+            DoubleBuffered = true;
+
+            spaces = new Space[rows * columns];
+
+            for (int i = 0; i < columns; i++)
+            {
+                for (int j = 0; j < rows; j++)
+                {
+                    int index = i * rows + j;
+                    spaces[index] = new Space(i * 40, j * 40);
+                }
+            }
+
+            Size = new Size(columns * 40 + 2, rows * 40 + 2);
+
+            thread = new Thread(new ThreadStart(onUpdate));
+            thread.Start();
+        }
+
+        public void onDraw(Object sender, PaintEventArgs e)
+        {
+            foreach (Space space in spaces)
+                space.onDraw(e.Graphics);
+        }
+
+        public void onUpdate()
+        {
+            while (true)
+            {
+                Invalidate();
+            }
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            float x = e.X;
+            float y = e.Y;
+            if (a == null)
+            {
+                foreach (Space space in spaces)
+                {
+                    if (space.getType() == SpaceType.None && space.contains(x, y))
+                    {
+                        a = space;
+                        a.setType(SpaceType.A);
+                        break;
+                    }
+                }
+            }
+            else if (b == null)
+            {
+                foreach (Space space in spaces)
+                {
+                    if (space.getType() == SpaceType.None && space.contains(x, y))
+                    {
+                        b = space;
+                        b.setType(SpaceType.B);
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                foreach (Space space in spaces)
+                {
+                    if (space.getType() == SpaceType.None && space.contains(x, y))
+                    {
+                        space.setType(SpaceType.Wall);
+                        break;
+                    }
+                }
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space)
+            {
+
+            }
+            if (e.KeyCode == Keys.Back)
+            {
+                a = b = null;
+                foreach (Space space in spaces)
+                {
+                    space.setType(SpaceType.None);
+                }
+            }
+        }
+    }
+}
---
+++ PathfindingAStar/Space.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace PathfindingAStar
+{
+    public enum SpaceType
+    {
+        None, Wall, A, B
+    }
+    class Space
+    {
+        float x, y;
+        int valueA, valueB;
+        bool active;
+        SpaceType type;
+
+        public Space(float x, float y, SpaceType type = SpaceType.None)
+        {
+            this.x = x;
+            this.y = y;
+            this.type = type;
+        }
+
+        public void onDraw(Graphics g)
+        {
+            switch(type)
+            {
+                case SpaceType.Wall:
+                    g.FillRectangle(Brushes.Blue, x, y, 40, 40);
+                    break;
+                case SpaceType.A:
+                    g.FillRectangle(Brushes.Green, x, y, 40, 40);
+                    break;
+                case SpaceType.B:
+                    g.FillRectangle(Brushes.Red, x, y, 40, 40);
+                    break;
+            }
+            g.DrawRectangle(Pens.Blue, x, y, 40, 40);
+
+            if (active)
+            {
+                Font font = new Font(new FontFamily("Arial"), 6);
+                g.DrawString((valueA + valueB).ToString(), font, Brushes.White, x + 4, y + 4);
+                g.DrawString(valueA.ToString(), font, Brushes.White, x + 4, y + 30);
+                g.DrawString(valueB.ToString(), font, Brushes.White, x + 30, y + 30);
+            }
+        }
+
+        public void setValue(int a, int b)
+        {
+            this.valueA = a;
+            this.valueB = b;
+        }
+
+        public void setType(SpaceType type)
+        {
+            this.type = type;
+        }
+
+        public SpaceType getType()
+        {
+            return type;
+        }
+
+        public bool contains(float x, float y)
+        {
+            if(x > this.x && x < this.x + 40
+            && y > this.y && y < this.y + 40)
+                return true;
+
+            return false;
+        }
+    }
+}
